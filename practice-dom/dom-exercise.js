1b0d5677538558612af6fd8dbb1aad4ddb3bc0c3
G984692019 <198469@st.takushoku-u.ac.jp>
2023-06-29 14:56:37 +0900

    修正

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -15,7 +15,12 @@ let data = [
 
 // 練習4-2 メッセージ追加プログラム
 
+let h2 = document.querySelector('h2#ex42');
 
+let p = document.createElement('p');
+p.textContent = '写真表と都市の緯度経度のページです'
+
+h2.insertAdjacentElement('afterend', p);
 // 練習4-3 写真表作成プログラム
 
 
