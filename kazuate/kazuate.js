0e5c2d53f6b157b328fb8b5ea49f3b37fff8892f
G984692019 <198469@st.takushoku-u.ac.jp>
2023-07-06 16:00:41 +0900

    修正

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -17,9 +17,10 @@ b1.addEventListener('click', hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+
   let i = document.querySelector('input[name="suji"]');
-  let nyuryoku = i.ariaValueMax;
-  let yoso = Math.floor(nyuryoku);
+  let nyuryoku = i.value;
+  let yoso = Number(nyuryoku);
 
   kaisu = kaisu + 1;
   console.log(kaisu + "回目の予想:" + yoso);
@@ -29,6 +30,7 @@ function hantei() {
 
   let h2 = document.querySelector('span#answer');
   h2.textContent = yoso;
+
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
