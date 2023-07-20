4b9d41f25e6ae71ccd7e5ca50bc8a169355aa14c
G984692019 <198469@st.takushoku-u.ac.jp>
2023-07-20 16:29:51 +0900

    多く　編集

--- weather.js
+++ weather.js
@@ -43,6 +43,47 @@ let data = {
   "cod": 200
 };
 
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
+
+function sendRequest(){
+  let rs = document.querySelectorAll('input[name="country"]');
+  for(let r of rs) {
+    if(r.checked){
+      let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + r.id + '.json';
+
+      axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
+    }
+  }
+}
+
+function showResult(resp) {
+  // サーバから送られてきたデータを出力
+  let data = resp.data;
+
+  // data が文字列型なら，オブジェクトに変換する
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+
+  // data をコンソールに出力
+  console.log(data);
+
+  // data.x を出力
+  console.log(data.x);
+}
+
+function showError(err) {
+  console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
 ////////// 課題3-2 ここからプログラムを書こう
 console.log(data.name);
 console.log(data.coord.lon);
