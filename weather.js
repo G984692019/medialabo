35cee1521fbdd21e196147277e902be9d6fd47ee
G984692019 <198469@st.takushoku-u.ac.jp>
2023-07-20 17:51:00 +0900

    色々編集

--- weather.js
+++ weather.js
@@ -61,6 +61,7 @@ function sendRequest(){
 }
 
 function showResult(resp) {
+  result.remove();
   // サーバから送られてきたデータを出力
   let data = resp.data;
 
@@ -88,6 +89,80 @@ function showResult(resp) {
   let row_1 = document.createElement('tr');
   let heading_1 = document.createElement('th');
   heading_1.innerHTML = data.name;
+  row_1.appendChild(heading_1);
+  tbody.appendChild(row_1);
+
+  let row_2 = document.createElement('tr');
+  let row_2_data_1 = document.createElement('td');
+  row_2_data_1.innerHTML = "緯度";
+  let row_2_data_2 = document.createElement('td');
+  row_2_data_2.innerHTML = data.coord.lon;
+  row_2.appendChild(row_2_data_1);
+  row_2.appendChild(row_2_data_2);
+  tbody.appendChild(row_2);
+
+  let row_3 = document.createElement('tr');
+  let row_3_data_1 = document.createElement('td');
+  row_3_data_1.innerHTML = "経度";
+  let row_3_data_2 = document.createElement('td');
+  row_3_data_2.innerHTML = data.coord.lat;
+  row_3.appendChild(row_3_data_1);
+  row_3.appendChild(row_3_data_2);
+  tbody.appendChild(row_3);
+
+  let row_4 = document.createElement('tr');
+  let row_4_data_1 = document.createElement('td');
+  row_4_data_1.innerHTML = "天気";
+  let row_4_data_2 = document.createElement('td');
+  row_4_data_2.innerHTML = data.weather.description;
+  row_4.appendChild(row_4_data_1);
+  row_4.appendChild(row_4_data_2);
+  tbody.appendChild(row_4);
+
+  let row_5 = document.createElement('tr');
+  let row_5_data_1 = document.createElement('td');
+  row_5_data_1.innerHTML = "最低気温";
+  let row_5_data_2 = document.createElement('td');
+  row_5_data_2.innerHTML = data.main.temp_min;
+  row_5.appendChild(row_5_data_1);
+  row_5.appendChild(row_5_data_2);
+  tbody.appendChild(row_5);
+
+  let row_6 = document.createElement('tr');
+  let row_6_data_1 = document.createElement('td');
+  row_6_data_1.innerHTML = "最高気温";
+  let row_6_data_2 = document.createElement('td');
+  row_6_data_2.innerHTML = data.main.temp_max;
+  row_6.appendChild(row_6_data_1);
+  row_6.appendChild(row_6_data_2);
+  tbody.appendChild(row_6);
+
+  let row_7 = document.createElement('tr');
+  let row_7_data_1 = document.createElement('td');
+  row_7_data_1.innerHTML = "湿度";
+  let row_7_data_2 = document.createElement('td');
+  row_7_data_2.innerHTML = data.main.humidity;
+  row_7.appendChild(row_7_data_1);
+  row_7.appendChild(row_7_data_2);
+  tbody.appendChild(row_7);
+
+  let row_8 = document.createElement('tr');
+  let row_8_data_1 = document.createElement('td');
+  row_8_data_1.innerHTML = "風速";
+  let row_8_data_2 = document.createElement('td');
+  row_8_data_2.innerHTML = data.wind.gust;
+  row_8.appendChild(row_8_data_1);
+  row_8.appendChild(row_8_data_2);
+  tbody.appendChild(row_8);
+
+  let row_9 = document.createElement('tr');
+  let row_9_data_1 = document.createElement('td');
+  row_9_data_1.innerHTML = "風向";
+  let row_9_data_2 = document.createElement('td');
+  row_9_data_2.innerHTML = data.wind.deg;
+  row_9.appendChild(row_9_data_1);
+  row_9.appendChild(row_9_data_2);
+  tbody.appendChild(row_9);
 
   document.getElementById('body').appendChild(table);
 }
